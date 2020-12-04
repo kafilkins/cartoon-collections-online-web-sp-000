97c245c7fe6fd77f64696583770dc51a710e8aa5
Keith Filkins <kafilkins@me.com>
2020-12-04 21:14:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,11 +19,11 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
   if array.include? == (cheese_types)
     true
   else
     false
   end
+  cheese_types = ["cheddar", "gouda", "camembert"]
 end
