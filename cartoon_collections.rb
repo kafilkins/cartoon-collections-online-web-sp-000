05319838f29c0a4a82f269ab55ef63972c75d1ac
Keith Filkins <kafilkins@me.com>
2020-12-04 21:14:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(array)
 
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
+  if array.include? == (cheese_types)
     true
   else
     false
