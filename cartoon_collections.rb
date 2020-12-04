5cf26ccc5c7040f6b9ca1374653defe161e4e411
Keith Filkins <kafilkins@me.com>
2020-12-04 20:25:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(names)
-  name.each_with_index do |names|
-  end
+  names.each_with_index do |name, index|
+    puts name
 end
 
 def summon_captain_planet# code an argument here
