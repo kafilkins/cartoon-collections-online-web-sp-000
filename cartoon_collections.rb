37d036fd75da6f7f00d39a3330b82c043d09dc1b
Keith Filkins <kafilkins@me.com>
2020-12-04 20:58:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,12 @@ def summon_captain_planet(planeteer_calls)
    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls > 4 
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
