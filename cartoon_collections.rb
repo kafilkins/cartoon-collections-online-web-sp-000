8e6406162abe61b07ceec675768d1a180baeede4
Keith Filkins <kafilkins@me.com>
2020-12-04 20:55:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |call|
-    call.capitalize + "!"
+    puts call.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
